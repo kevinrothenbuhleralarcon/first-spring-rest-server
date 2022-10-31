64aaad6015ec310976e2d012f71196792fae9af5
KevinRothenbühler-Al <kevin.rothenbuhler-alarcon@softcom.pro>
2022-10-31 17:22:38 +0100

    Add form to add a new employee

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { EmployeeComponent } from './employee/employee.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { EmployeeComponent } from './employee/employee.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/src/app/employee/employee.component.ts
+++ front/src/app/employee/employee.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {EmployeeService} from "../employee.service";
-import {first, Observable, startWith, Subject, switchMap} from "rxjs";
+import {BehaviorSubject, first, Observable, switchMap} from "rxjs";
 import {Employee} from "../model/Employee";
 
 @Component({
@@ -10,27 +10,31 @@ import {Employee} from "../model/Employee";
 })
 export class EmployeeComponent {
 
-  employeesSubject$ = new Subject();
+  employeesSubject$ = new BehaviorSubject(null);
   employees$: Observable<Employee[]>;
+  newEmployee = new Employee();
 
   constructor(private employeeService: EmployeeService) {
     this.employees$ = this.employeesSubject$.asObservable().pipe(
-      startWith(null),
       switchMap(() => this.employeeService.getEmployees())
     );
   }
 
   addEmployee() {
-    const employee = new Employee();
-    employee.firstName = "Kevin";
-    employee.lastName = "Rothenbühler-Alarcon";
-    employee.role = "Developer";
-
-    this.employeeService.addEmployee(employee).pipe(first()).subscribe(() => this.employeesSubject$.next(null));
+    if (this.validateEmployee()) {
+      this.employeeService.addEmployee(this.newEmployee).pipe(first()).subscribe(() =>  {
+        this.newEmployee = new Employee();
+        this.employeesSubject$.next(null);
+      });
+    }
   }
 
   remove(id: number) {
     this.employeeService.deleteEmployee(id).pipe(first()).subscribe(() => this.employeesSubject$.next(null));
+  }
+
+  private validateEmployee(): boolean {
+    return !(this.newEmployee.firstName === "" || this.newEmployee.lastName === "" || this.newEmployee.role === "");
   }
 
 }
